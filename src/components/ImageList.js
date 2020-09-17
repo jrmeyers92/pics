ac73485a4001d0710cd2f24f65274f5f04e8294e
Jake Meyers <jakejake92@yahoo.com>
2020-09-16 19:45:43 -0600

    refactor

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,8 +1,8 @@
 import React from "react";
 
 const ImageList = (props) => {
-    const images = props.images.map((image) => {
-        return <img alt = {image.alt} key = {image.id}  src={image.urls.regular} />
+    const images = props.images.map(({description, urls, id}) => {
+        return <img alt = {description}  key = {description}  src={urls.regular} />
     } ) ;
     
     return (
